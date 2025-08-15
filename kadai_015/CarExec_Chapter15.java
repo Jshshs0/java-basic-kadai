61d49c8aaa88c75b219db2d3b70785a8ab3a8db8
yudai-hirano <yudaihirano0@gmail.com>
2025-08-15 15:20:55 +0900

    kadai_015

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -1,5 +1,13 @@
 package kadai_015;
 
 public class CarExec_Chapter15 {
-
+    public static void main(String[] args) {
+    	
+    	// ギアデータを生成
+    	Car_Chapter15 gearspeed = new Car_Chapter15(1, 10);
+    	
+    	// ギア３の場合
+    	gearspeed.changeGear(3);
+    	gearspeed.run();
+    }
 }
--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -1,5 +1,49 @@
 package kadai_015;
 
 public class Car_Chapter15 {
+	// フィールド名
+	private int gear  = 1;  // ギア
+    private int speed = 10; // スピード
+	
+	// 【コンストラクタ】
+	public Car_Chapter15(int gear, int speed) {
+		this.gear  = gear;
+		this.speed = speed;
+	}
+	
+	// 【メソッド】
+	public void changeGear( final int afterGear) {
+		switch(afterGear) {
+		case 1 -> {
+			gear  = 1;
+			speed = 10;
+		           }
+		case 2 -> {
+			gear  = 2;
+			speed = 20;
+		           }
+		case 3 -> {
+			gear  = 3;
+			speed = 30;
+		           }
+		case 4 -> {
+			gear  = 4;
+			speed = 40;
+		           }
+		case 5 -> {
+			gear  = 5;
+			speed = 50;
+		           }
+		default -> {
+			gear  = 1;
+			speed = 10;
+		            }
+		}
+	}
+	
+	public void run() {
+		System.out.println(gear + "に切り替わりました");
+		System.out.println("速度は時速" + speed + "kmです");
+	}
 
-}
+}	
