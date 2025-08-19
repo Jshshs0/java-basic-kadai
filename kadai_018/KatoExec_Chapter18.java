7fd10fae3a1c96bab27d73ca6a37fd495e1b8bd9
yudai-hirano <yudaihirano0@gmail.com>
2025-08-19 14:51:05 +0900

    kadai_018

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,24 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+	
+	public static void main(String[] args) {
+		
+		// KatoTaro_Chapter18クラスのオブジェクトを作る
+		KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+		taro.setGivenName();
+		taro.execIntroduce();
+		
+		// KatoIchiro_Chapter18クラスのオブジェクトを作る
+		KatoIchiro_Chapter18 ichro = new KatoIchiro_Chapter18();
+		ichro.setGivenName();
+		ichro.execIntroduce();
+		
+		// KatoHanako_Chapter18クラスのオブジェクトを作る
+		KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
+		hanako.setGivenName();
+		hanako.execIntroduce();
+		
+	}
+
+}
---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,15 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18 {
+	
+	// 名を表すフィールドの値をセットする
+		public void setGivenName() {
+			super.givenName = "花子";
+		}
+		
+		//
+		public void eachIntroduce() {
+			System.out.println("Javaが得意です");
+		}
+
+}
